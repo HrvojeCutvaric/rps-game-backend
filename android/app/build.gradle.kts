969f281f9e9372e0eadb8713354aa381bf057c39
HrvojeCutvaric <111574558+HrvojeCutvaric@users.noreply.github.com>
2026-01-09 08:36:06 +0100

    add dependencies for koin, navigation3, and retrofit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,4 +57,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.bundles.koin)
+    implementation(libs.bundles.navigation3)
+    implementation(libs.bundles.retrofit)
 }
